85d23fcb04e7a5ce4feb20d0f5385bca0f6a2a19
Josie <JoshAlfordJoseph@gmail.com>
2023-05-10 12:04:41 -0400

    initial commit

---
+++ SFApi/src/main/kotlin/com/example/SFApi/SfApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.SFApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SfApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SfApiApplication>(*args)
+}
---
+++ SFApi/src/test/kotlin/com/example/SFApi/SfApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SFApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SfApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
